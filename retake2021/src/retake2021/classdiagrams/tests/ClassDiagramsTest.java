2f4ef892771d18c58e4bd79e0ad3244573892e3e
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2021-07-26 11:16:51 +0200

    Offical test suite

--- retake2021/src/retake2021/classdiagrams/tests/ClassDiagramsTest.java
+++ retake2021/src/retake2021/classdiagrams/tests/ClassDiagramsTest.java
@@ -12,28 +12,68 @@ import retake2021.classdiagrams.Multiplicity;
 
 class ClassDiagramsTest {
 
+	ClassBox studentBox0 = new ClassBox("Student");
+	ClassBox studentBox = new ClassBox("Student");
+	ClassBox teamBox = new ClassBox("Team");
+	AssociationLine line = new AssociationLine(studentBox, "team", Multiplicity.ZERO_TO_ONE, teamBox, "member", Multiplicity.ZERO_TO_MANY);
+	
 	@Test
-	void test() {
-		ClassBox studentBox = new ClassBox("Student");
+	void testClassBoxGetName() {
 		assertEquals("Student", studentBox.getName());
-		assertEquals(Map.of(), studentBox.getRoles());
-		
-		ClassBox teamBox = new ClassBox("Team");
-		AssociationLine line = new AssociationLine(studentBox, "team", Multiplicity.ZERO_TO_ONE, teamBox, "member", Multiplicity.ZERO_TO_MANY);
+	}
+	
+	@Test
+	void testGetRolesInitially() {
+		assertEquals(Map.of(), studentBox0.getRoles());
+	}
+	
+	@Test
+	void testIsDeleted() {
 		assertFalse(line.isDeleted());
+	}
+	
+	@Test
+	void testGetStartRole() {
 		assertTrue(line.getStartRole() != null);
+	}
+	
+	@Test
+	void testGetClassBox() {
 		assertEquals(studentBox, line.getStartRole().getClassBox());
-		assertEquals("team", line.getStartRole().getName());
-		assertEquals(Multiplicity.ZERO_TO_ONE, line.getStartRole().getMultiplicity());
-		assertEquals(line, line.getStartRole().getAssociationLine());
-		assertTrue(line.getEndRole() != null);
 		assertEquals(teamBox, line.getEndRole().getClassBox());
+	}
+	
+	@Test
+	void testRoleGetName() {
+		assertEquals("team", line.getStartRole().getName());
 		assertEquals("member", line.getEndRole().getName());
+	}
+	
+	@Test
+	void testGetMultiplicity() {
+		assertEquals(Multiplicity.ZERO_TO_ONE, line.getStartRole().getMultiplicity());
 		assertEquals(Multiplicity.ZERO_TO_MANY, line.getEndRole().getMultiplicity());
+	}
+	
+	@Test
+	void testGetAssociationLine() {
+		assertEquals(line, line.getStartRole().getAssociationLine());
 		assertEquals(line, line.getEndRole().getAssociationLine());
+	}
+	
+	@Test
+	void testGetEndRole() {
+		assertTrue(line.getEndRole() != null);
+	}
+
+	@Test
+	void testGetRoles() {
 		assertEquals(Map.of("team", line.getStartRole()), studentBox.getRoles());
 		assertEquals(Map.of("member", line.getEndRole()), teamBox.getRoles());
-		
+	}
+	
+	@Test
+	void testDelete() {
 		line.delete();
 		assertTrue(line.isDeleted());
 		assertEquals(Map.of(), studentBox.getRoles());
--- retake2021/src/retake2021/maps/tests/MapTest.java
+++ retake2021/src/retake2021/maps/tests/MapTest.java
@@ -2,13 +2,9 @@ package retake2021.maps.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
 import retake2021.maps.EmptyMap;
-import retake2021.maps.Map;
 import retake2021.maps.NonemptyMap;
 
 class MapTest {
@@ -19,30 +15,58 @@ class MapTest {
 			EmptyMap.of()));
 	
 	@Test
-	void test() {
+	void testGetKey() {
 		assertEquals("één", myMap.getKey());
-		assertEquals("one", myMap.getValue());
-		assertEquals(NonemptyMap.of("twee", "two", EmptyMap.of()), myMap.getTail());
-		assertEquals("one", myMap.get("één"));
-		assertEquals("two", myMap.get("twee"));
-		assertEquals(null, myMap.get("drie"));
-		assertFalse(myMap.equals(EmptyMap.of()));
-		assertFalse(EmptyMap.of().equals(myMap));
-		assertFalse(myMap.equals(myMap.getTail()));
 	}
 	
 	@Test
-	void testExtraPointsRequirements() {
-		Set<String> keys = new HashSet<>();
-		for (String key : myMap)
-			keys.add(key);
-		assertEquals(Set.of("één", "twee"), keys);
-		
-		Set<String> values = new HashSet<>();
-		myMap.forEachValue(value -> values.add(value));
-		assertEquals(Set.of("one", "two"), values);
-		
-		assertEquals(Set.of(4), myMap.getLongKeyLengths());
+	void testGetValue() {
+		assertEquals("one", myMap.getValue());
 	}
+	
+	@Test
+	void testGetTail() {
+		assertEquals(NonemptyMap.of("twee", "two", EmptyMap.of()), myMap.getTail());
+	}
+	
+	@Test
+	void testGetExistingEntry() {
+		assertEquals("one", myMap.get("één"));
+		assertEquals("two", myMap.get("twee"));
+	}
+	
+	@Test
+	void testGetNonexisitingEntry() {
+		assertEquals(null, myMap.get("drie"));
+	}
+	
+	@Test
+	void testEqualsEmpty() {
+		assertFalse(myMap.equals(EmptyMap.of()));
+	}
+	
+	@Test
+	void testEmptyEquals() {
+		assertFalse(EmptyMap.of().equals(myMap));
+	}
+	
+	@Test
+	void testEqualsNonempty() {
+		assertFalse(myMap.equals(myMap.getTail()));
+	}
+	
+//	@Test
+//	void testExtraPointsRequirements() {
+//		Set<String> keys = new HashSet<>();
+//		for (String key : myMap)
+//			keys.add(key);
+//		assertEquals(Set.of("één", "twee"), keys);
+//		
+//		Set<String> values = new HashSet<>();
+//		myMap.forEachValue(value -> values.add(value));
+//		assertEquals(Set.of("one", "two"), values);
+//		
+//		assertEquals(Set.of(4), myMap.getLongKeyLengths());
+//	}
 
 }
